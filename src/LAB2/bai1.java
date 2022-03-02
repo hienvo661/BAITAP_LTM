9adfaf63003057c99d600ebf48882671a0c6721a
Võ Ngọc Hiện <hienvo661@gmail.com>
2022-03-02 19:42:51 +0700

    2

---
+++ src/LAB2/bai1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package LAB2;
+
+/**
+ * @author ACER
+ *
+ */
+public class bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
